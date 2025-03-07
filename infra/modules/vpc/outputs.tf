958e27268b640ea6ac25883633899eebc0d15554
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-03-06 23:46:20 -0300

    creating vpc, public and private subnets, internet gateway and route table of internet gateway

---
+++ infra/modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID da VPC"
+  value       = aws_vpc.ArchFacts_Main_VPC
+}
+
+output "public_subnet_ids" {
+  description = "IDs das subnets PÚBLICAS"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs das subnets PRIVADAS"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_id" {
+  description = "ID do Nat Gateway"
+  value       = var.nat_gateway ? aws_nat_gateway.this[0].id : null
+}

5e13788e206ea24d25ed63879c1b4dcc32b3659c
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-03-07 10:47:28 -0300

    finishing route tables and associations of internet gateway and nat gateway

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -56,3 +56,51 @@ resource "aws_route_table" "ig_route_table" {
     Name        = "ig_route_table-${var.vpc_name}"
   }
 }
+
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.ig_route_table.id
+}
+
+#IP ELÁSTICO OBRIGATÓRIO PARA O NAT_GATEWAY
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+    Name        = "eip-nat-gateway-${var.vpc_name}"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+    Name        = "nat_gateway_public_subnet-${var.vpc_name}"
+  }
+}
+
+resource "aws_route_table" "nat_route_table" {
+  vpc_id = aws_vpc.ArchFacts_Main_VPC.id
+
+  route {
+    cidr_block     = "0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+    Name        = "nat_route_table-${var.vpc_name}"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.nat_route_table
+}

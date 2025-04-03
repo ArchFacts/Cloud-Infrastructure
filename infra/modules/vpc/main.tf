be99926aea0a1918c61ccd58ee6991e4de8a6c66
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-04-03 10:58:34 -0300

    adding ACl and SG for EC2

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -109,3 +109,53 @@ resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.nat_route_table[count.index].id
 }
+
+resource "aws_network_acl" "main_acl" {
+  vpc_id = aws_vpc.ArchFacts_Main_VPC.id
+
+  ingress {
+    rule_no    = 100
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    rule_no    = 200
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  egress {
+    rule_no    = 300
+    action     = "allow"
+    protocol   = "-1"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+    Name        = "acl-${var.vpc_name}"
+  }
+}
+
+resource "aws_network_acl_association" "public_acl_association" {
+  count          = length(var.public_subnet_cidrs)
+  network_acl_id = aws_network_acl.main_acl.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+}
+
+resource "aws_network_acl_association" "private_acl_association" {
+  count          = length(var.public_subnet_cidrs)
+  network_acl_id = aws_network_acl.main_acl.id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+}

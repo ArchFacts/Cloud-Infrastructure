2221c052925ff2193c70bb71dc1da5f43cec764b
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-04-18 12:29:03 -0300

    adding last changes in security module

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "public_ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -79,6 +86,15 @@ resource "aws_network_acl" "public_acl" {
     cidr_block = "0.0.0.0/0"
     from_port  = 80
     to_port    = 80
+  }
+
+  ingress {
+    rule_no    = 115
+    action     = "allow"
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 5000
+    to_port    = 5000
   }
 
 
